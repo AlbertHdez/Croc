8ca9fe703161a29751917de114b2cba9c01653f0
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-10-25 22:43:31 -0600

    nav html

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-	include_once('php/views/home.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Croc</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css">
+	<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Aleo:wght@700&family=Lato:wght@300&family=Montserrat:wght@200;300;400;500;600&family=Oswald:wght@200;400;500&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="src/css/main.css">
+</head>
+<body id="home">
+	<?php include_once ('php/components/nav.php');?>
+	<span>Papalas</span>
+</body>
+</html>
---
+++ php/components/nav.php
@@ -0,0 +1,27 @@
+<nav class="nav">
+	<div class="nav__social">
+		<a href="" class="nav__icon-link">
+			<span class="iconify nav__icon" data-icon="mdi:facebook" data-inline="false"></span>
+		</a>
+		<a href="" class="nav__icon-link">
+			<span class="iconify nav__icon" data-icon="mdi:twitter" data-inline="false"></span>
+		</a>
+		<a href="" class="nav__icon-link">
+			<span class="iconify nav__icon" data-icon="mdi:youtube" data-inline="false"></span>
+		</a>
+	</div>
+	<div class="nav__search">
+		<span class="iconify nav__icon" data-icon="mdi:magnify" data-inline="false"></span>
+	</div>
+	<a href="" class="nav__brand">
+		<img src="src/img/Logo.svg" alt="" class="nav__logo">
+	</a>
+	<div class="nav__links--left">
+		<a href="" class="nav__link"></a>
+		<a href="" class="nav__link"></a>
+	</div>
+	<div class="nav__links--right">
+		<a href="" class="nav__link"></a>
+		<a href="" class="nav__link"></a>
+	</div>
+</nav>
