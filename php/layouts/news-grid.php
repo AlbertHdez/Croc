459603dc5b83b196eed294aea42bedb1a9621dca
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-10-28 14:25:19 -0600

    news grid created

---
+++ php/layouts/news-grid.php
@@ -0,0 +1,59 @@
+<section class="news-grid">
+	<a href="" class="news-grid__principal">
+		<div class="news-grid__background">
+			<img src="https://picsum.photos/1200/800" alt="" class="news-grid__image">
+		</div>
+		<section class="news-grid__article">
+			<p class="news-grid__tag">
+				Reporte
+			</p>
+			<p class="news-grid__title">
+				Noticia principal
+			</p>
+		</section>
+	</a>
+	<section class="news-grid__others">
+		<a href="" class="news-grid__new">
+			
+				<p class="news-grid__tag">
+					Reporte
+				</p>
+				<p class="news-grid__title">
+					Noticia principal
+				</p>
+			
+		</a>
+		<a href="" class="news-grid__new">
+			
+				<p class="news-grid__tag">
+					Reporte
+				</p>
+				<p class="news-grid__title">
+					Noticia principal
+				</p>
+			
+		</a>
+		<a href="" class="news-grid__secondary">
+			<div class="news-grid__background">
+				<img src="https://picsum.photos/1200/800" alt="" class="news-grid__image">
+			</div>
+			<section class="news-grid__article">
+				<p class="news-grid__tag">
+					Reporte
+				</p>
+				<p class="news-grid__title">
+					Noticia principal
+				</p>
+			</section>
+		</a>
+		<a href="" class="news-grid__new">
+			
+				<p class="news-grid__tag">
+					Reporte
+				</p>
+				<p class="news-grid__title">
+					Noticia principal
+				</p>
+		</a>
+	</section>
+</section>
