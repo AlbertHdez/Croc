720ab2ab46cd25e4b28dda187e44f899296f3179
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-10-29 10:24:14 -0600

    values was created

---
+++ php/layouts/values.php
@@ -0,0 +1,50 @@
+<section class="values">
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+	<div class="values__value" style="background-image: url('https://picsum.photos/400')">
+		<div class="values__content">
+			<p class="values__text">Cabecera de Valores</p>
+			<img src="https://picsum.photos/100" alt="" class="values__icon">
+		</div>
+	</div>
+</section>
