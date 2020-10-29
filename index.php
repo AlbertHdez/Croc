a3b1225e3ae701dfe0aadfe9e5e24815c60f897c
AlbertHdez <mariohdez_proyectos@outlook.com>
2020-10-29 12:38:56 -0600

    new item was created

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" href="src/css/main.css">
 </head>
 <body id="home">
-	<?php include_once ('php/components/simple-new.php');?>
+	<?php include_once ('php/components/new-item.php');?>
 	
 </body>
 </html>
